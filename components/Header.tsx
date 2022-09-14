60bbfbb965dd3f5dd3dcbf3c0f8df06c78d8c9d1
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-09-13 23:41:53 -0600

    updated font weight

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header class="px-3 py-3 bg-yellow-200 h-16 flex items-center">
       <div class="px-4 max-w-screen-md">
-        <a href="/" class="text-2xl hover:text-underline">
+        <a href="/" class="text-2xl font-bold hover:text-underline">
           {site.title}
         </a>
       </div>
