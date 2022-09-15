20deae462496811008da92204e975fa048c5fb71
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-09-15 00:02:08 -0600

    added 404 page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,16 +3,18 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/blog/[slug].tsx";
-import * as $2 from "./routes/index.tsx";
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/_app.tsx";
+import * as $2 from "./routes/blog/[slug].tsx";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/_app.tsx": $0,
-    "./routes/blog/[slug].tsx": $1,
-    "./routes/index.tsx": $2,
+    "./routes/_404.tsx": $0,
+    "./routes/_app.tsx": $1,
+    "./routes/blog/[slug].tsx": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/_404.tsx
@@ -0,0 +1,33 @@
+import { Header } from '../components/Header.tsx';
+
+export function ServerCodePage(
+  props: { serverCode: number; codeDescription: string },
+) {
+  return (
+    <>
+    <Header/>
+      <section class="w-full flex items-center justify-center">
+        <div class="text-center">
+          <h1 class="text-6xl md:text-9xl font-extrabold">
+            {props.serverCode}
+          </h1>
+
+          <p class="p-4 text-2xl md:text-3xl">
+            {props.codeDescription}
+          </p>
+
+          <p class="p-4">
+            <a href="/" class="hover:underline">Back to the Homepage</a>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default function PageNotFound() {
+  return ServerCodePage({
+    serverCode: 404,
+    codeDescription: "We couldn't find the page you're looking for.",
+  });
+}
