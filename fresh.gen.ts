50c2448226c099911fd79b63812e1e67a5c77e38
Thomas Hansen <31554741+thomas992@users.noreply.github.com>
2023-07-23 15:32:37 -0400

    update dependencies
    preact, fresh, replace front matter with std

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/blog/[slug].tsx";
@@ -20,7 +19,6 @@ const manifest = {
     "./islands/Counter.tsx": $$0,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
