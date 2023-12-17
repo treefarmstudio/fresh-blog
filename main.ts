2c828b660e51457e3134c24820a3eb6390560a23
Thomas Hansen <tomhansen992@gmail.com>
2023-12-17 14:46:30 -0500

    Using Tailwind and update dependencies

--- main.ts
+++ main.ts
@@ -7,7 +7,6 @@
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, { plugins: [tailwind()] });
